1e0423f1e2b96839e2277c283645de2812539099
Nawroz Salehi <salehi.nawroz@gmail.com>
2023-03-24 15:10:40 -0400

    lesson_1_1 branch

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div style={{ textAlign: 'center' }}>
       <header>
         <p>
-          Edit <code>src/App.js</code> and save to reload. this is fro branch 1.1
+          Edit <code>src/App.js</code> and save to reload. this is fro branch 1.1adfdsfds
         </p>
         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
